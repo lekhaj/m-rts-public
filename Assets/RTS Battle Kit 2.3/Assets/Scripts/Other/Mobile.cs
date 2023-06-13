9022e4800155849495fc6bd5a7017e2bb8a8a1fa
YeshKrish <yeshwinkrishna@gmail.com>
2023-06-14 00:31:17 +0530

    Task 2 Camera angles (Created both Camera Setups)

--- Assets/RTS Battle Kit 2.3/Assets/Scripts/Other/Mobile.cs
+++ Assets/RTS Battle Kit 2.3/Assets/Scripts/Other/Mobile.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Mobile : MonoBehaviour {
 	
@@ -26,106 +27,137 @@ public class Mobile : MonoBehaviour {
 		switchSelectionModeButton = GameObject.Find("Switch selection mode button");
 		buttons = GameObject.Find("Buttons");
 	}
-	
-	void Update(){
+
+	void Update() {
 		//check if you can move the camera
-		if(camEnabled){
-			
-		//use the move booleans to know when to move the camera
-		if(moveLeft){
-			Camera.main.transform.Translate(Vector3.right * Time.deltaTime * -CamController.movespeed);
-		}
-		if(moveRight){
-			Camera.main.transform.Translate(Vector3.right * Time.deltaTime * CamController.movespeed);
-		}
-		if(moveUp){
-			Camera.main.transform.Translate(Vector3.up * Time.deltaTime * CamController.movespeed);
-		}
-		if(moveDown){
-			Camera.main.transform.Translate(Vector3.up * Time.deltaTime * -CamController.movespeed);
-		}
-		}
-		
-		//turn the buttons of when any of the menu's is active
-		if(!Manager.StartMenu.activeSelf && !Manager.victory && !Manager.gameOver && !Settings.settingsMenu.activeSelf){
-			buttons.SetActive(true);
-		}
-		else{
-			buttons.SetActive(false);
-		}
-		
-		//Set mobile selection mode button active/not active
-		if(CharacterManager.selectionMode){
-			switchSelectionModeButton.SetActive(true);
-		}
-		else{
-			switchSelectionModeButton.SetActive(false);
-		}
-	}
-	
-	//start moving the camera based on the direction
-	public void moveCameraButtonDown(string direction){
-		if(direction == "right"){
-			moveRight = true;
-		}
-		if(direction == "left"){
-			moveLeft = true;
-		}
-		if(direction == "up"){
-			moveUp = true;
-		}
-		if(direction == "down"){
-			moveDown = true;
-		}
-	}
-	
-	//stop moving the camera based on direction
-	public void moveCameraButtonUp(string direction){
-		if(direction == "right"){
-			moveRight = false;
-		}
-		if(direction == "left"){
-			moveLeft = false;
-		}
-		if(direction == "up"){
-			moveUp = false;
-		}
-		if(direction == "down"){
-			moveDown = false;
-		}
-	}
-	
-	//toggle deploymode on/off
-	public void toggleDeployMode(){
-		deployMode = !deployMode;
-		if(deployMode){
-			//set button color to red
-			GameObject.Find("Deploy units button").GetComponent<Image>().color = Color.red;
-			if(CharacterManager.selectionMode){
-				//switch selection mode off
-				GameObject.Find("Manager").GetComponent<CharacterManager>().selectCharacters();
+		if (camEnabled)
+		{
+
+			if (SceneManager.GetActiveScene().name == "Single player Clash Royal")
+			{
+				Debug.Log("Clash Royal");
+				if (moveLeft)
+				{
+					Camera.main.transform.Translate(Vector3.right * Time.deltaTime * -CamController.movespeed);
+				}
+				if (moveRight)
+				{
+					Camera.main.transform.Translate(Vector3.right * Time.deltaTime * CamController.movespeed);
+				}
+				if (moveUp)
+				{
+					Camera.main.transform.Translate(Vector3.up * Time.deltaTime * CamController.movespeed);
+				}
+				if (moveDown)
+				{
+					Camera.main.transform.Translate(Vector3.up * Time.deltaTime * -CamController.movespeed);
+				}
 			}
-			//camera off
-			camEnabled = false;
+			else if (SceneManager.GetActiveScene().name == "Single player Castle Crush")
+			{
+				Debug.Log("Castle Crush");
+				if (moveLeft)
+				{
+					Camera.main.transform.Translate(Vector3.right * Time.deltaTime * -CamController.movespeed);
+				}
+				if (moveRight)
+				{
+					Camera.main.transform.Translate(Vector3.right * Time.deltaTime * CamController.movespeed);
+				}
+				if (moveUp)
+				{
+					Camera.main.transform.Translate(Vector3.forward * Time.deltaTime * CamController.movespeed);
+				}
+				if (moveDown)
+				{
+					Camera.main.transform.Translate(Vector3.forward * Time.deltaTime * -CamController.movespeed);
+				}
+
+				//use the move booleans to know when to move the camera
+			}
 		}
-		else{
-			//set button color to white
-			GameObject.Find("Deploy units button").GetComponent<Image>().color = Color.white;
-			//camera on
-			camEnabled = true;
+
+			//turn the buttons of when any of the menu's is active
+			if (!Manager.StartMenu.activeSelf && !Manager.victory && !Manager.gameOver && !Settings.settingsMenu.activeSelf) {
+				buttons.SetActive(true);
+			}
+			else {
+				buttons.SetActive(false);
+			}
+
+			//Set mobile selection mode button active/not active
+			if (CharacterManager.selectionMode) {
+				switchSelectionModeButton.SetActive(true);
+			}
+			else {
+				switchSelectionModeButton.SetActive(false);
+			}
+		}
+
+		//start moving the camera based on the direction
+		public void moveCameraButtonDown(string direction) {
+			if (direction == "right") {
+				moveRight = true;
+			}
+			if (direction == "left") {
+				moveLeft = true;
+			}
+			if (direction == "up") {
+				moveUp = true;
+			}
+			if (direction == "down") {
+				moveDown = true;
+			}
+		}
+
+		//stop moving the camera based on direction
+		public void moveCameraButtonUp(string direction) {
+			if (direction == "right") {
+				moveRight = false;
+			}
+			if (direction == "left") {
+				moveLeft = false;
+			}
+			if (direction == "up") {
+				moveUp = false;
+			}
+			if (direction == "down") {
+				moveDown = false;
+			}
+		}
+
+		//toggle deploymode on/off
+		public void toggleDeployMode() {
+			deployMode = !deployMode;
+			if (deployMode) {
+				//set button color to red
+				GameObject.Find("Deploy units button").GetComponent<Image>().color = Color.red;
+				if (CharacterManager.selectionMode) {
+					//switch selection mode off
+					GameObject.Find("Manager").GetComponent<CharacterManager>().selectCharacters();
+				}
+				//camera off
+				camEnabled = false;
+			}
+			else {
+				//set button color to white
+				GameObject.Find("Deploy units button").GetComponent<Image>().color = Color.white;
+				//camera on
+				camEnabled = true;
+			}
+		}
+
+		//switch between selection modes (box select & move units mode)
+		public void switchSelectionMode() {
+			selectionModeMove = !selectionModeMove;
+
+			//change the sprite displayed
+			if (selectionModeMove) {
+				switchSelectionModeButton.GetComponent<Image>().sprite = moveUnits;
+			}
+			else {
+				switchSelectionModeButton.GetComponent<Image>().sprite = selectUnits;
+			}
 		}
 	}
-	
-	//switch between selection modes (box select & move units mode)
-	public void switchSelectionMode(){
-		selectionModeMove = !selectionModeMove;
-		
-		//change the sprite displayed
-		if(selectionModeMove){
-			switchSelectionModeButton.GetComponent<Image>().sprite = moveUnits;
-		}
-		else{
-			switchSelectionModeButton.GetComponent<Image>().sprite = selectUnits;
-		}
-	}
-}
+
